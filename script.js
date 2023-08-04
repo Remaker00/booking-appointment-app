1f1c0296768b400c8863ec82be16faec58be28d2
Nishant Sourav <nishant.sharma8507966@gmail.com>
2023-08-04 13:47:07 +0530

    version1.1

--- script.js
+++ script.js
@@ -36,8 +36,9 @@ function addItem(e){
         .catch((err) => {
           console.log(err)
         })
-        
+
     //localStorage.setItem(descrip,JSON.stringify (exp));
+    
   
     document.getElementById('item1').value = '';
     document.getElementById('item2').value = '';
@@ -49,15 +50,40 @@ function addItem(e){
       document.getElementById('item3').value = categ;
     })
 }
+window.addEventListener("DOMContentLoaded", () => {
+  axios.get("https://crudcrud.com/api/b5835a91e64b4134bdef72b51e5cea62/appointmentData")
+  .then((response) => {
+    console.log(response);
+    response.data.forEach((appointment) => {
+      var li = document.createElement('li');
+      var itms = appointment.amnt + '-' + appointment.descrip + '-' + appointment.categ + ' ';
+      li.appendChild(document.createTextNode(itms));
 
-  function removeitm(e){
-    if(e.target.textContent === 'Delete Expense' || e.target.textContent === 'Edit Expense'){
-      var li= e.target.parentElement;
-      var itemText = li.textContent.trim(); 
-      var email = itemText.split('-')[1].trim();
-      localStorage.removeItem(email);
-      itemlist.removeChild(li);
-    }
+      var delbtn = document.createElement('button');
+      delbtn.appendChild(document.createTextNode('Delete Expense'));
+      li.appendChild(delbtn);
+
+      var edtbtn = document.createElement('button');
+      edtbtn.appendChild(document.createTextNode('Edit Expense'));
+      li.appendChild(edtbtn);
+
+      itemlist.appendChild(li);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+    
+  })
+
+function removeitm(e){
+  if(e.target.textContent === 'Delete Expense' || e.target.textContent === 'Edit Expense'){
+     var li= e.target.parentElement;
+     var itemText = li.textContent.trim(); 
+     var email = itemText.split('-')[1].trim();
+     localStorage.removeItem(email);
+     itemlist.removeChild(li);
   }
+}
   
  
